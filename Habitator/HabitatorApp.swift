0b6adbc39cfacce1e2a2bf8c787f22c8235be976
dalithop <zhaoxiong.ang@gmail.com>
2021-11-12 21:03:06 +0800

    Initial Commit

---
+++ Habitator/HabitatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitatorApp.swift
+//  Habitator
+//
+//  Created by Nyein Nyein on 12/11/21.
+//
+
+import SwiftUI
+
+@main
+struct HabitatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
