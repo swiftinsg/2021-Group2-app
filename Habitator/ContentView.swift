a880b5aac4df97bc2d0def040e3a1434a7da5dbd
dalithop <zhaoxiong.ang@gmail.com>
2021-11-15 11:02:27 +0800

    added menu barr

--- Habitator/ContentView.swift
+++ Habitator/ContentView.swift
@@ -7,10 +7,53 @@
 
 import SwiftUI
 
+struct ActionWord{
+    var singular: String
+    var Plural: String
+}
+
+struct Habit{
+    var verb: String
+    var action: ActionWord
+}
+
 struct ContentView: View {
+    var selectedHabit: Habit?
+    var appPurple=UIColor(rgb: 0x766CD1)
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView() {
+            Text("[INSERT HOME VIEW]")
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }
+                .tag(0)
+         
+            Text("[INSERT HABITS VIEW]")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Habits")
+                }
+                .tag(1)
+         
+            Text("[INSERT PROGRESS VIEW]")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .tabItem {
+                    Image(systemName: "chart.bar")
+                    Text("Progress")
+                }
+                .tag(2)
+         
+            Text("[INSERT GOALS VIEW]")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .tabItem {
+                    Image(systemName: "target")
+                    Text("Goals")
+                }
+                .tag(3)
+        }
+        .accentColor(Color(appPurple))
     }
 }
 
