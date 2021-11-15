77b8729945d63f3b3b8f5273e7e55c7a109b976a
dalithop <zhaoxiong.ang@gmail.com>
2021-11-15 12:24:46 +0800

    add home screen thingy??!?!?!??!

---
+++ Habitator/HomeView.swift
@@ -0,0 +1,22 @@
+//
+//  HomeView.swift
+//  Habitator
+//
+//  Created by Nyein Nyein on 15/11/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @Binding var currentHabit: Habit?
+    @Binding var habits: [Habit]
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
