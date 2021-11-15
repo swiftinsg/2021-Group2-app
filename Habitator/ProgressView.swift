d3bddefe39eaabca1fb9d517b8124c689b019699
dalithop <zhaoxiong.ang@gmail.com>
2021-11-15 12:27:55 +0800

    Fixed progressView stufff????/1/??!1//!/?1//1/!?

--- Habitator/ProgressView.swift
+++ Habitator/ProgressView.swift
@@ -7,27 +7,21 @@
 
 import SwiftUI
 
-struct progressView: View{
+struct ProgressView: View{
     @State var Progress: Progress
     var body: some View {
         VStack(){
             Text("Progress")
-                .font(.system(size: 50, .weight: bold))
-            }
+                .font(.system(size: 50, weight: .bold))
         }
     }
-let testProgress=[
-    Habit(
-        Text("Progress")
-            .font(.system(size: 50, .weight: bold))
-    )
-]
+}
  
 struct ProgressView: View {
     @Binding var currentProgress: Progress?
     var body: some View {
         List {
-            progressView()
+            ProgressView()
         }
     }
 }
