5a0d3ac5f3b7c947faab73ae05e8a378562af2ba
shezan2 <shezankhn@hotmail.com>
2021-11-16 10:34:05 +0800

    copying stuff from habits to goals and editing to start it off

--- Habitator/GoalsView.swift
+++ Habitator/GoalsView.swift
@@ -7,29 +7,86 @@
 
 import SwiftUI
 
-struct GoalView: View{
-    @State var goal: Goal
-    @Binding var habit: Habit
-    var body: some View{
-        HStack{
-            Text(goal.name)
-            if (goal.isAcheived(habit: habit)){
-                Image(uiImage: UIImage(systemName:"checkmark")!)
+struct GoalsListItemView: View{
+    @State var habit: Habit
+    @Binding var selected: Habit?
+    @Binding var habits: [Habit]
+    var body: some View {
+        Button(action: {selected=habit}){
+            HStack(){
+                Text(habit.name)
+                if ((selected != nil) && selected!.name==habit.name){
+                    Spacer()
+                    Image(uiImage: UIImage(systemName:"checkmark")!)
+                }
             }
         }
+    }
+}
+
+struct PlaceholderGoalsView: View{
+    @Binding var habits:[Habit]
+    @Binding var text: String
+    var body: some View {
+        TextField(
+            "Type To Enter",
+            text: $text
+        ).opacity(0.5)
     }
 }
 
 struct GoalsView: View {
     @Binding var currentHabit: Habit?
     @Binding var habits: [Habit]
+    @State var placeholderText=""
+    @State var editing=false
     var body: some View {
-        if ((currentHabit) != nil){
+        VStack{
+            ZStack{
+                Text("Habits")
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                HStack{
+                    Button("Edit"){
+                        editing = !editing
+                    }.padding(.leading)
+                    Spacer()
+                }
+            }
+            Divider()
             List{
-                ForEach(currentHabit!.goals,id: \.name){ goal in
-                    //goal is in goal
+                ForEach(habits,id: \.name){ habit in
+                    HabitListItemView(
+                        habit: habit,
+                        selected: $currentHabit,
+                        habits: $habits
+                    )
+                }
+                Button(action: {
+                    if (!(placeholderText=="")){
+                        habits+=[Habit(
+                            object: ObjectWord(
+                                singular: "undefined",
+                                plural: "undefined"
+                            ),
+                            action: "undefined",
+                            name: placeholderText, goals: []
+                        )]
+                        placeholderText=""
+                    }
+                }){
+                    PlaceholderHabitView(
+                        habits: $habits,
+                        text: $placeholderText
+                    )
                 }
             }
         }
     }
 }
+
+
+//struct HabitsView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        HabitsView()
+//    }
+//}
