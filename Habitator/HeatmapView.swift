47e7da57b1f998034c18a9bd61cea41b5e5a6267
dalithop <zhaoxiong.ang@gmail.com>
2021-11-19 10:31:58 +0800

    add meat map

---
+++ Habitator/HeatmapView.swift
@@ -0,0 +1,18 @@
+//
+//  HeatmapView.swift
+//  Habitator
+//
+//  Created by Nyein Nyein on 19/11/21.
+//
+
+import SwiftUI
+
+struct HeatmapView: View {
+    @Binding var current: Int?
+    @Binding var habits: [Habit]
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
